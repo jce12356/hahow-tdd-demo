8ac8414fe4b1bedcfdf90dc419205ff7f54c4ea8
JasonLee <jasonlee@lafresh.com.tw>
2024-10-20 00:15:08 +0800

    update main/test_main

--- models.py
+++ models.py
@@ -1,12 +1,11 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import String, Column, Integer, DATETIME
+from sqlalchemy import String, Column, Integer, DateTime  # 修正這裡
 from datetime import datetime
 
 Base = declarative_base()
-
 
 class Board(Base):
     __tablename__ = "board"
     id: int = Column(Integer, primary_key=True, autoincrement=True)
     name: str = Column(String(64))
-    created_time: datetime = Column(DATETIME)
+    created_time: datetime = Column(DateTime)  # 修正這裡
